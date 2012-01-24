e75cc1d7a32046e6359f078a6e7d7c9bf1a42677
Paul Forti <Paul Forti>
2012-01-24 13:23:05 +0100

    Reforge complète des structures de données

--- src/model/Column.java
+++ src/model/Column.java
@@ -1,19 +1,23 @@
 package model;
-import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Column extends ArrayList{
+public class Column extends HashMap<Integer, Object>{
 	
 	private String label;
 	private Object type;
+	private Integer index;
 	
 	public Column(String label)
 	{
 		this.label = label;
+		this.index = new Integer(0);
 	}
 	
 	public Column(String label, Object dataType)
 	{
 		this.label = label;
 		this.type = dataType;
+		this.index = new Integer(0);
 	}
+	
 }
--- src/model/Table.java
+++ src/model/Table.java
@@ -1,34 +1,18 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.lang.Integer;
-public class Table<indexO, columns> extends HashMap<Integer, Column> {
-	
-	private Integer index;
+
+public class Table extends ArrayList<Column> {
 	
 	public Table()
 	{
-		this.index = new Integer(0);
+		
 	}
 	
 	public Table(String[] columnNames)
 	{
-		this.index = new Integer(0);
-		this.create(columnNames, index);
-	}
-	
-	private void create(String[] columnNames, Integer indexRow)
-	{
-		for (String s : columnNames)
-		{
-			System.out.println("Création d'une colonne avec un label : "+s);
-			Column buffCol = new Column(s);
-			this.put(indexRow, buffCol);
-		}
-	}
-	
-	public void add(Object abstractType, Integer indexRow)
-	{
-		Column buffCol = this.get(indexRow);
+		
 	}
 }
