e2607eb0acb4d7e73b4d70028227483e2713ae92
Paul Forti <paul.forti@gmail.com>
2012-01-24 12:17:54 +0100

    Ajout du constructeur générique de colonne avec type

--- src/model/Column.java
+++ src/model/Column.java
@@ -2,11 +2,18 @@ package model;
 import java.util.ArrayList;
 
 public class Column extends ArrayList{
+	
 	private String label;
+	private Object type;
 	
 	public Column(String label)
 	{
 		this.label = label;
 	}
 	
+	public Column(String label, Object dataType)
+	{
+		this.label = label;
+		this.type = dataType;
+	}
 }
