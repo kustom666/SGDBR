b1d7cd03899eb9261919f0912fe39180246cae0c
Paul Forti <Paul Forti>
2012-01-24 11:42:43 +0100

    Ajout de l'index de table

--- src/model/Table.java
+++ src/model/Table.java
@@ -1,13 +1,19 @@
 package model;
 
 import java.util.HashMap;
-
+import java.lang.Integer;
 public class Table<Integer, Column> extends HashMap<Integer, Column> {
 	
-	private Integer index;
+	private int index;
 	
 	public Table()
 	{
-		
+		index = 0;
+	}
+	
+	public Table(String[] columnNames)
+	{
+		index = 0;
+		Integer indexO = new Integer(index);
 	}
 }
