c20694156e7d3ee5b2a9e00aaed2fcea73b0fe0a
Paul Forti <paul.forti@gmail.com>
2012-02-07 11:15:56 +0100

    Résolutions de warnings

--- src/model/Column.java
+++ src/model/Column.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Column extends ArrayList<Types>{
 	
+	private static final long serialVersionUID = 1437340961489074002L;
 	private String label;
 	private Types type;
 	
--- src/model/Line.java
+++ src/model/Line.java
@@ -3,5 +3,7 @@ package model;
 import java.util.ArrayList;
 
 public class Line extends ArrayList<Types>{
+
+	private static final long serialVersionUID = 1395631029655406828L;
 	
 }
