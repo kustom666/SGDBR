a83af819892c57ee67e3c58f8f2a5295a79a4e08
Paul Forti <paul.forti@gmail.com>
2012-02-07 11:15:56 +0100

    Résolutions de warnings

--- src/model/Table.java
+++ src/model/Table.java
@@ -3,7 +3,9 @@ package model;
 import java.util.ArrayList;
 
 public class Table extends ArrayList<Line> {
-	
+
+	private static final long serialVersionUID = 5771048924784085348L;
+
 	public Table()
 	{
 		
