33dc0f90b86313e4f047a7456d179a9370d1476f
Paul Forti <paul.forti@gmail.com>
2012-02-07 11:12:51 +0100

    Suppression de l'index dans la colonne (l'index étant le type primaire de la table, il est inutile d'indexer sous forme de hashmap)

--- src/model/Column.java
+++ src/model/Column.java
@@ -6,19 +6,16 @@ public class Column extends ArrayList<Types>{
 	
 	private String label;
 	private Types type;
-	private Integer index;
 	
 	public Column(String label)
 	{
 		this.label = label;
-		this.index = new Integer(0);
 	}
 	
 	public Column(String label, Types dataType)
 	{
 		this.label = label;
 		this.type = dataType;
-		this.index = new Integer(0);
 	}
 	
 }
