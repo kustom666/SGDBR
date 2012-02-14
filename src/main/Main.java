d99abdd1bd2b5c2c1d9ab0078d5601f2b7db5af5
Paul Forti <paul.forti@gmail.com>
2012-02-14 11:51:19 +0100

    Modification du constructeur de table

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,18 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import types.Types;
+import types.Integer;
+import model.Table;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		ArrayList<Types> al = new ArrayList<Types>();
+		al.add(new Integer());
+		Table buffTable = new Table(args, al);
 
 	}
 
--- src/model/Table.java
+++ src/model/Table.java
@@ -1,18 +1,20 @@
 package model;
 
 import java.util.ArrayList;
-
+import types.Types;
 public class Table extends ArrayList<Line> {
 
 	private static final long serialVersionUID = 5771048924784085348L;
+	private ArrayList<Column> arrCol;
 
-	public Table()
+	public Table(String[] columnNames, ArrayList<Types> listType)
 	{
-		
-	}
-	
-	public Table(String[] columnNames)
-	{
-		
+		int i = 0;
+		for(String s : columnNames){
+			Column buffCol = new Column(s, listType.get(i));
+			arrCol.add(buffCol);
+			i++;
+			System.out.println("Ajout d'une colonne ayant pour label : " + s);
+		}
 	}
 }
