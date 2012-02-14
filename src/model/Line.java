b61b1f6ae75cf3ce5b76345c9ba2e85f0a2022d3
Paul Forti <paul.forti@gmail.com>
2012-02-14 09:30:48 +0100

    Creation du type Integer, ajout des contraintes de clefs sur les types

--- src/model/Line.java
+++ src/model/Line.java
@@ -2,6 +2,8 @@ package model;
 
 import java.util.ArrayList;
 
+import types.Types;
+
 public class Line extends ArrayList<Types>{
 
 	private static final long serialVersionUID = 1395631029655406828L;
