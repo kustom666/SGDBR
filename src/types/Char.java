bad1c64fd2f3e6217f1ab60e29ff28d63274153e
Paul Forti <paul.forti@gmail.com>
2012-02-14 10:11:19 +0100

    Ajout du type char

---
+++ src/types/Char.java
@@ -0,0 +1,20 @@
+package types;
+
+public class Char extends Types{
+	private char val[];
+	private int maxLength;
+	
+	public Char(char valI[], int length){
+		this.val = valI;
+		this.maxLength = length;
+	}
+
+	public char[] getVal() {
+		return val;
+	}
+
+	public void setVal(char[] val) {
+		this.val = val;
+	}
+	
+}
