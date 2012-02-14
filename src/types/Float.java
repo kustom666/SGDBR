69700ff321ebcf042d5cb6bb510234e675b37ba2
Paul Forti <paul.forti@gmail.com>
2012-02-14 09:50:24 +0100

    Finalisation du type float

---
+++ src/types/Float.java
@@ -0,0 +1,19 @@
+package types;
+
+public class Float extends Types{
+	private float val;
+	
+	public Float(float valInc){
+		this.val = valInc;
+	}
+
+	public float getVal() {
+		return val;
+	}
+
+	public void setVal(float val) {
+		this.val = val;
+	}
+	
+	
+}
