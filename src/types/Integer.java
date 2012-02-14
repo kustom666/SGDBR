ff17ac5bbbad82a9c7707c9ff12c7e92e4372103
Paul Forti <paul.forti@gmail.com>
2012-02-14 09:47:04 +0100

    Finalisation du type integer

--- src/types/Integer.java
+++ src/types/Integer.java
@@ -1,5 +1,18 @@
 package types;
 
 public class Integer extends Types {
+	private boolean autoIncrement;
+	private int val;
+	
+	public Integer(int valInc){
+		this.val = valInc;
+	}
 
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
 }
