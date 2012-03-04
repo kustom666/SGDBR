03f2b8c5237ccaadfc2104b528921de7d2618380
Paul Forti <paul.forti@gmail.com>
2012-03-04 14:58:51 +0100

    Mise a jour de tous les alias

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,15 +3,16 @@ package main;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import types.Sinteger;
 import types.Types;
-import types.Integer;
+import types.Sinteger;
 import model.Table;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<Types> al = new ArrayList<Types>();
-		al.add(new Integer());
+		al.add(new Sinteger());
 		Table buffTable = new Table(args, al);
 
 	}
