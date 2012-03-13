2d50da3eca6ac3736d7e25f37fa8fd22706ef8d7
Paul Forti <paul.forti@gmail.com>
2012-03-13 09:21:35 +0100

    Ajout de la classe base

--- src/controller/Crud.java
+++ src/controller/Crud.java
@@ -1,5 +1,23 @@
 package controller;
+import model.*;
+import types.*;
 
 public class Crud {
-
+	
+	private Table usedTable;
+	
+	public Crud()
+	{
+		
+	}
+	
+	public Crud(Table t)
+	{
+		this.usedTable = t;
+	}
+	
+	public void setUsedTable(Table t)
+	{
+		this.usedTable = t;
+	}
 }
