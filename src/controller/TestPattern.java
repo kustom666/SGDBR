1566536e2ef01c72af92ccb6d4e136291e0e6b23
Tony Coriolle <tony.coriolle@gmail.com>
2012-04-24 17:12:04 +0200

    modification minime des Patterns

--- src/controller/TestPattern.java
+++ src/controller/TestPattern.java
@@ -9,15 +9,16 @@ public class TestPattern {
 	private Instruction instruction=new Instruction();
 
 	public Table test(String i) throws TestPatternException{
-		Pattern create=Pattern.compile("^CREATE TABLE [a-zA-Z0-9]* \\([[_a-zA-Z0-9]* [A-Za-z]*,]*\\);$",Pattern.MULTILINE);
-		Pattern alterAdd=Pattern.compile("^ALTER TABLE [a-zA-Z0-9]* ADD [a-zA-Z0-9]* [a-zA-Z]*;$");
-		Pattern alterDrop=Pattern.compile("^ALTER TABLE [a-zA-Z0-9]* DROP [a-zA-Z]*;$");
-		Pattern alterChange=Pattern.compile("^ALTER TABLE [a-zA-Z0-9]* CHANGE [a-zA-Z]* [a-zA-Z]* [a-zA-Z]*;$");
-		Pattern alterModify=Pattern.compile("^ALTER TABLE [a-zA-Z0-9]* MODIFY [a-zA-Z]* [a-zA-Z]*;$");
-		Pattern insertInto=Pattern.compile("^INSERT INTO [a-zA-Z0-9]* \\([[a-zA-Z0-9]*,]*\\) VALUES \\([[a-zA-Z0-9.]*,]*\\);$",Pattern.MULTILINE);
-		Pattern selectFrom=Pattern.compile("^SELECT [a-zA-Z0-9]* FROM [a-zA-Z0-9]*;$",Pattern.MULTILINE);
-		//Pattern deleteFrom=Pattern.compile("^DELETE FROM [a-zA-Z0-9]* WHERE \\{[a-zA-Z0-9] [<
-		Pattern update=Pattern.compile("^UPDATE [a-zA-Z0-9]* SET [a-zA-Z0-9=.]* WHERE [a-zA-Z0-9.=<>!]*;$", Pattern.MULTILINE);
+		Pattern create=Pattern.compile("^CREATE TABLE [a-zA-Z0-9]*\\s*\\([[_a-zA-Z0-9]* [A-Za-z]*\\s*,]*\\);$",Pattern.MULTILINE);
+		Pattern alterAdd=Pattern.compile("^ALTER TABLE [a-zA-Z0-9]* ADD [a-zA-Z0-9]* [a-zA-Z]*;$",Pattern.MULTILINE);
+		Pattern alterDrop=Pattern.compile("^ALTER TABLE [a-zA-Z0-9]* DROP [a-zA-Z0-9]*;$",Pattern.MULTILINE);
+		Pattern alterChange=Pattern.compile("^ALTER TABLE [a-zA-Z0-9]* CHANGE [a-zA-Z0-9]* [a-zA-Z0-9]* [a-zA-Z]*;$",Pattern.MULTILINE);
+		Pattern alterModify=Pattern.compile("^ALTER TABLE [a-zA-Z0-9]* MODIFY [a-zA-Z0-9]* [a-zA-Z]*;$",Pattern.MULTILINE);
+		Pattern insertInto=Pattern.compile("^INSERT INTO [a-zA-Z0-9]*\\s*\\([[a-zA-Z0-9]*\\s*,]*\\) VALUES\\s\\([[a-zA-Z0-9./\"]*\\s*,]*\\);$",Pattern.MULTILINE);
+		
+		Pattern selectFrom=Pattern.compile("^SELECT [[a-zA-Z0-9_]*\\s*,]* FROM [a-zA-Z0-9]*\\s*;$"/*WHERE [a-zA-Z0-9.=<>!\\s*]*;$"*/,Pattern.MULTILINE);
+		Pattern deleteFrom=Pattern.compile("^DELETE FROM [a-zA-Z0-9]*\\s*;$"/*WHERE [a-zA-Z0-9!=<>\\s*]*;$"*/, Pattern.MULTILINE);
+		Pattern update=Pattern.compile("^UPDATE [a-zA-Z0-9]* SET [a-zA-Z0-9]* = [a-zA-Z0-9.\"/]*;$"/*WHERE [a-zA-Z0-9.=<>!\\s*]*;$"*/, Pattern.MULTILINE);
 
 		boolean reponseCreate =create.matcher(i).matches();
 		boolean reponseAlterAdd =alterAdd.matcher(i).matches();
@@ -27,7 +28,7 @@ public class TestPattern {
 		boolean reponseInsert =insertInto.matcher(i).matches();
 		boolean reponseUpdate =update.matcher(i).matches();
 		boolean reponseSelect= selectFrom.matcher(i).matches();
-		//boolean reponseDelete=deleteFrom.matcher(i).matches();
+		boolean reponseDelete=deleteFrom.matcher(i).matches();
 
 
 		if (reponseCreate){
@@ -55,9 +56,9 @@ public class TestPattern {
 		else if(reponseSelect){
 			temp=instruction.SelectFrom(i);
 		}
-		/*else if(reponseDelete){
-			temp=instruction.deleteFrom(i);
-		}*/
+		else if(reponseDelete){
+			temp=instruction.DeleteFrom(i);
+		}
 		else {
 			throw new TestPatternException();
 		}
